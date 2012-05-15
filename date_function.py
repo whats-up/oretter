612eb2d5fa08f2696cacdc23645ec15e6841dbdd
whats-up <liontaxi@gmail.com>
2012-05-15 23:28:14 +0900

    twitter年齢その3
    変数名わかりにくいので修正した。

--- date_function.py
+++ date_function.py
@@ -3,16 +3,16 @@
 import datetime
 date=datetime.date(2010,11,1)
 
-def twitter_age(_date):
-    if isinstance(_date,datetime.datetime):
-        _date=_date.date()
+def twitter_age(created_at):
+    if isinstance(created_at,datetime.datetime):
+        created_at=created_at.date()
     d1=datetime.date(2008,4,28)#twitter日本語版サービス開始日
     now=datetime.date.today()
     max_age=now-d1
-    this_age=now-_date
+    this_age=now-created_at
     return int(1.0*this_age.days/max_age.days*100)
 
 def main():
-    pass
+    print twitter_age(date)
 if __name__ == '__main__':
     main()
